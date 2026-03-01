eb9bb79ee274e68601a5c7ff11060aff0b13d027
JahidWebDev <webdeveloper2324@gmail.com>
2026-03-01 16:40:29 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
-import Solutions from "./Pages/Solutions";
-import Industries from "./Pages/Industries";
-import Technical from "./Pages/Technical";
+
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Footer from "./Pages/Footer";
